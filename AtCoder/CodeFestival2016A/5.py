76987e8c7c806bf0f96f90602e37819f10935ed4
Yuxi <soration2099@gmail.com>
2016-10-04 20:23:24 +0900

    Update Atcode Code Festival roundA 5.py

--- AtCoder/CodeFestival2016A/5.py
+++ AtCoder/CodeFestival2016A/5.py
@@ -3,6 +3,12 @@
 # E-LRU Puzle
 def reads(offset = 0):
 	return [int(i) - offset for i in input().split(' ')]
+def Judge(vector):
+	length = len(vector)-1
+	for i in range(length):
+		if(vector[i] > vector[i+1]):		
+			return 0
+	return 1
 #Data input
 (N, M) = reads()
 Q = int(input())
@@ -13,36 +19,29 @@ pos = [-1] * M
 pat = []
 freq = [0] * (M+1)
 freq[0] = N
+found = set()
 
 #Pattern search & Match count
 counter = 0
 for i in A[::-1]:
-	if (i not in pat):
+	if (i not in found):
 		pat.append(i)
+		found.add(i)
 		pos[i] = counter
 		freq[counter] -= 1
 		counter += 1
 		freq[counter] += 1
-	else:
-		temp = pos[i]
-		if (freq[temp] > 0):
-			freq[temp] -= 1
-			freq[temp+1] += 1
+	elif (freq[pos[i]] > 0):
+		freq[pos[i]] -= 1
+		freq[pos[i]+1] += 1
 
-if(counter != M):
-	for i in range(M):
-		if (i not in pat):
-			pat.append(i)
+for i in range(M):
+	if (pos[i] == -1):
+		pat.append(i)
 
 #Result judge
-for i in range(M):
+for i in range(M+1):
 	if (freq[i] != 0):
 		start = i
 		break
-
-p = pat[start:]
-p.sort()
-if(p == pat[start:]):
-	print("Yes")
-else:
-	print("No")
+print("Yes") if Judge(pat[start:]) else print("No")
