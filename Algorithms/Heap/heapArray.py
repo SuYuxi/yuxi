6fa549615627bbc3c8e2b8f41ba5f4b5177da284
Yuxi <soration2099@gmail.com>
2017-06-06 01:21:27 +0900

    Heap

--- Algorithms/Heap/heapArray.py
+++ Algorithms/Heap/heapArray.py
@@ -7,55 +7,57 @@ class Heap(object):
 		return self._stack
 
 	@value.setter
-	def val(self, value):
+	def value(self, value):
 		self._stack = value
 		return self._stack
 
 	def _shiftUp(self, inx):
 		cur = inx + 1
 		upper = cur // 2
-		while(cur && _stack[cur-1] < _stack[upper-1]):
-			_stack[cur-1], _stack[upper-1] = _stack[upper-1], _stack[cur-1]
+		while(cur and self._stack[cur-1] < self._stack[upper-1]):
+			self._stack[cur-1], self._stack[upper-1] = self._stack[upper-1], self._stack[cur-1]
 			cur = upper
 			upper = cur // 2
 
 	def _shiftDown(self, inx):
-		size = len(_stack)
+		size = len(self._stack)
 		cur = inx
 		lower = cur * 2 + 1 
 		while(lower < size):
-			if(lower+1 < size and _stack[lower] > _stack[lower+1]):
+			if(lower+1 < size and self._stack[lower] > self._stack[lower+1]):
 				lower += 1
-			if(_stack[cur] > _stack[lower]):
-				_stack[cur], _stack[lower] = _stack[lower], _stack[cur] 
+			if(self._stack[cur] > self._stack[lower]):
+				self._stack[cur], self._stack[lower] = self._stack[lower], self._stack[cur] 
 			else:
 				break
+			cur = lower
+			lower = cur * 2 + 1 
 				
 	def pop(self):
-		if(x):
-			top = x[0]
-			x[0] = x[-1]
-			x = x[:-1]
-			_shiftDown(0)
+		if(self._stack):
+			top = self._stack[0]
+			self._stack[0] = self._stack[-1]
+			self._stack = self._stack[:-1]
+			self._shiftDown(0)
 			return top
 
 	def push(self, value):
-		_stack.append(value)
-		_shiftUp(-1)
-		return self._stack
+		self._stack.append(value)
+		self._shiftUp(-1)
+		return self.self._stack
 
 	def replace(self, value):
-		if(x):
-			top = x[0]
-			x[0] = value
-			_shiftDown(0)
+		if(self._stack):
+			top = self._stack[0]
+			self._stack[0] = value
+			self._shiftDown(0)
 			return top
 		
 	def top(self):
-		if(x):
-			return x[0]
+		if(self._stack):
+			return self._stack[0]
 
 	def heapify(self):
 		for inx in range((len(self._stack) // 2) - 1, -1, -1):
-			_shiftDown(inx)
+			self._shiftDown(inx)
 		return self._stack
