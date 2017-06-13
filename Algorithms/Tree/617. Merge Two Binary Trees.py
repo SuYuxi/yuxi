2c9a3caeba0ceca3e05d39e54606dc5f514bc553
yuxi <soration2099@gmail.com>
2017-06-13 22:29:40 +0900

    performance improved

--- Algorithms/Tree/617. Merge Two Binary Trees.py
+++ Algorithms/Tree/617. Merge Two Binary Trees.py
@@ -7,20 +7,11 @@
 
 class Solution(object):
 	def mergeTrees(self, t1, t2):
-		def preorder(node1, node2):
-			node1.val += node2.val
-			if(node2.left):
-				if(node1.left):
-					preorder(node1.left, node2.left)
-				else:
-					node1.left = node2.left
-			if(node2.right):
-				if(node1.right):
-					preorder(node1.right, node2.right)
-				else:
-					node1.right = node2.right
-		if(not t1):	
-			t1 = t2
-		elif(t1 and t2):
-			preorder(t1, t2)
+		if(not t1):
+			return t2
+		if(not t2):
+			return t1
+		t1.val += t2.val
+		t1.left = self.mergeTrees(t1.left, t2.left)
+		t1.right = self.mergeTrees(t1.right, t2.right)
 		return t1
