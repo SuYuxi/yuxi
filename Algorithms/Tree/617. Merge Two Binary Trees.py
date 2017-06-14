8396ae24969253d6be9401097dccaca970969f0e
yuxi <soration2099@gmail.com>
2017-06-14 15:56:24 +0900

    iterative added

--- Algorithms/Tree/617. Merge Two Binary Trees.py
+++ Algorithms/Tree/617. Merge Two Binary Trees.py
@@ -15,3 +15,27 @@ class Solution(object):
 		t1.left = self.mergeTrees(t1.left, t2.left)
 		t1.right = self.mergeTrees(t1.right, t2.right)
 		return t1
+
+
+class Solution(object):
+	def mergeTrees(self, t1, t2):
+		if(not t1):
+			return t2
+		if(not t2):
+			return t1
+		stack = list()
+		stack.append((t1, t2))
+		while(stack):
+			cur = stack.pop()	
+			cur[0].val += cur[1].val
+			if(not cur[0].right):
+				if(cur[1].right):
+					cur[0].right = cur[1].right
+			elif(cur[1].right):
+				stack.append((cur[0].right, cur[1].right))
+			if(not cur[0].left):
+				if(cur[1].left):
+					cur[0].left = cur[1].left
+			elif(cur[1].left):
+				stack.append((cur[0].left, cur[1].left))
+		return t1
