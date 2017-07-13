bf1211a213a73fb41cc9893a61718ecd52986ed4
Yuxi <soration2099@gmail.com>
2017-07-13 23:48:15 +0900

    sudoku

--- Algorithms/Backtracking/37. Sudoku Solver.py
+++ Algorithms/Backtracking/37. Sudoku Solver.py
@@ -1,5 +1,22 @@
+#Fastest
+class Cell(object):
+	def __init__(self, value = 0):
+		self.value = value
+		self.possibility = 9
+		self.bits = [0] * 9
+
 class Solution(object):
-    def isValidSudoku(self, board, row, col, val ):
+	cells = [[Cell() for _ in range(9)] for _ in range(9)]
+	def set(self, row, col, val):
+		if
+	def constraint(self, row, col, val):
+		
+    def isValidSudoku(self, board, row, col, val):
+		
+
+#Brute-Force Backtracking
+class Solution(object):
+    def isValidSudoku(self, board, row, col, val):
 		r, c = 3 * (row // 3), 3 * (col // 3)
 		for inx in range(9):
 			if(board[row][inx] == val): return False
