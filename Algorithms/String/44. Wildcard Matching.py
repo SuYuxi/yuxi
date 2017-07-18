0d275db005982f1621776ecb1d8e6eff03d9c1aa
yuxi <soration2099@gmail.com>
2017-07-18 21:25:56 +0900

    wildcard matching

--- Algorithms/String/44. Wildcard Matching.py
+++ Algorithms/String/44. Wildcard Matching.py
@@ -1,3 +1,4 @@
+#Great idea
 class Solution(object):
 	def isMatch(self, s, p):
 		lenS, lenP = len(s), len(p)
@@ -21,3 +22,20 @@ class Solution(object):
 		while(j < lenP and p[j] == '*'):
 			j += 1
 		return True if j == lenP else False
+
+#Fantastic idea
+class Solution:
+	def isMatch(self, s, p):
+		length = len(s)
+		if len(p) - p.count('*') > length:
+			return False
+		dp = [True] + [False]*length
+		for i in p:
+			if i != '*':
+				for n in reversed(range(length)):
+					dp[n+1] = dp[n] and (i == s[n] or i == '?')
+			else:
+				for n in range(1, length+1):
+					dp[n] = dp[n-1] or dp[n]
+			dp[0] = dp[0] and i == '*'
+		return dp[-1]
