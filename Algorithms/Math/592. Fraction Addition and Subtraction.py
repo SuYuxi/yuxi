d2e4af9da6973a029f5dd95158a42bc56ebb8d58
yuxi <soration2099@gmail.com>
2017-09-04 17:34:14 +0900

    greatest common divisor

--- Algorithms/Math/592. Fraction Addition and Subtraction.py
+++ Algorithms/Math/592. Fraction Addition and Subtraction.py
@@ -1,7 +1,20 @@
 class Solution(object):
     def fractionAddition(self, expression):
-        """
-        :type expression: str
-        :rtype: str
-        """
-        
+		def getGcd(a, b):
+			if(a == 0 or b == 0): return a + b
+			return getGcd(b, a%b)
+
+		begin, end = 0, 1
+		A, B = 0, 1
+		expression += '+'
+		for c in expression[1:]:
+			if(c == '-' or c == '+'):
+				a, b = map(int, expression[begin:end].split('/'))
+				A = a * B + A * b
+				B *= b
+				begin = end
+			end += 1
+		gcd = getGcd(A, B)
+		A //= gcd
+		B //= gcd
+		return str(A) + '/' + str(B)
