eb29e31e8a02166751274d60e2b4fd7ab79ef7fb
yuxi <soration2099@gmail.com>
2019-05-06 01:07:16 +0900

    update

--- Algorithms/Classical/SegmentTree.cpp
+++ Algorithms/Classical/SegmentTree.cpp
@@ -1,3 +1,4 @@
+//https://www.jianshu.com/p/91f2c503e62f?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
 //Segment Tree
 //range min query
 //array implementation
@@ -8,38 +9,60 @@
 using namespace std;
 class SegmentTree{
 public:
-	void build(const vector<int>& A) {
+	SegmentTree(const vector<int>& A) {
 		size = A.size();
-		segmentTree = vector<int>(2 * size, INT_MAX);
-		for(int i = 0; i < size; i++)
+		if(size > 0)
 		{
-			update(i, A[i]);
+			segmentTree = vector<int>(4 * size, INT_MAX);
+			build(A, 0, size - 1, 0);
 		}
+	}
+	void build(const vector<int>& A, int start, int end, int inx) {
+		if(start == end)
+		{
+			segmentTree[inx] = A[start];
+			return;
+		}
+		int mid = (start + end) / 2;
+		build(A, start, mid, 2 * inx + 1);
+		build(A, mid + 1, end, 2 * inx + 2);
+		segmentTree[inx] = min(segmentTree[2 * inx + 1], segmentTree[2 * inx + 2]);
 	};
 
-	void update(int inx, int value) {
-		int i = size - 1 + inx;
-		segmentTree[i] = value;
-		while(i > 0)
-		{
-			i = (i - 1) / 2;
-			segmentTree[i] = min(segmentTree[2 * i + 1], segmentTree[2 * i + 2]);
-		}
+	void update(int i, int value) {
+		update(0, size - 1, i, value, 0);
 	};
 
-	int query(int start, int end) {
-		return query(start, end, 0, size - 1, 0);
+	int query(int i, int j) {
+		return query(0, size - 1, i, j, 0);
 	};
 
 private:
-	int query(int start, int end, int left, int right, int inx) {
-		if(right < start || end < left) return INT_MAX; 
-		if(start <= left && right <= end) return segmentTree[inx];
-		int mid = (left + right) / 2;
-		return min(query(start, end, left, mid, inx * 2 + 1), query(start, end, mid + 1, right, inx * 2 + 2));
+	void update(int start, int end, int i, int value, int inx) {
+		if(start == end)
+		{
+			segmentTree[inx] = value;
+			return;
+		}
+		int mid = (start + end) / 2;
+		if(i <= mid)
+		{
+			update(start, mid, i, value, 2 * inx + 1);
+		}
+		else
+		{
+			update(mid + 1, end, i, value, 2 * inx + 2);
+		}
+		segmentTree[inx] = min(segmentTree[2 * inx + 1], segmentTree[2 * inx + 2]);
+	}
+
+	int query(int start, int end, int i, int j, int inx) {
+		if(start > j || end < i) return INT_MAX;
+		if(i <= start && j >= end) return segmentTree[inx];
+		int mid = (start + end) / 2;
+		return min(query(start, mid, i, j, 2 * inx + 1), query(mid + 1, end, i, j, 2 * inx + 2)); 
 	}
 
 	vector<int> segmentTree;
 	int size;
-
 };
--- Program Contest/AtCoder/いろはちゃんコンテスト Day1/G - 友達以上恋人以下.cpp
+++ Program Contest/AtCoder/いろはちゃんコンテスト Day1/G - 友達以上恋人以下.cpp
@@ -43,3 +43,112 @@ int main() {
 	cout << max(dfs(0, M), -1LL) << endl;
 	return 0;
 }
+
+//dp + rmq
+//https://img.atcoder.jp/iroha2019-day1/editorial-G.pdf
+#include <iostream>
+#include <vector>
+#include <cmath>
+const int INF = 1e12;
+
+using namespace std;
+
+class SegmentTree{
+public:
+	SegmentTree(const vector<long long>& A) {
+		size = A.size();
+		if(size > 0)
+		{
+			segmentTree = vector<long long>(4 * size, -INF);
+			build(A, 0, size - 1, 0);
+		}
+	}
+	void build(const vector<long long>& A, int start, int end, int inx) {
+		if(start == end)
+		{
+			segmentTree[inx] = A[start];
+			return;
+		}
+		int mid = (start + end) / 2;
+		build(A, start, mid, 2 * inx + 1);
+		build(A, mid + 1, end, 2 * inx + 2);
+		segmentTree[inx] = max(segmentTree[2 * inx + 1], segmentTree[2 * inx + 2]);
+	};
+
+	void update(int i, long long value) {
+		update(0, size - 1, i, value, 0);
+	};
+
+	long long query(int i, int j) {
+		return query(0, size - 1, i, j, 0);
+	};
+
+private:
+	void update(int start, int end, int i, long long value, int inx) {
+		if(start == end)
+		{
+			segmentTree[inx] = value;
+			return;
+		}
+		int mid = (start + end) / 2;
+		if(i <= mid)
+		{
+			update(start, mid, i, value, 2 * inx + 1);
+		}
+		else
+		{
+			update(mid + 1, end, i, value, 2 * inx + 2);
+		}
+		segmentTree[inx] = max(segmentTree[2 * inx + 1], segmentTree[2 * inx + 2]);
+	}
+
+	long long query(int start, int end, int i, int j, int inx) {
+		if(start > j || end < i) return -INF;
+		if(i <= start && j >= end) return segmentTree[inx];
+		int mid = (start + end) / 2;
+		return max(query(start, mid, i, j, 2 * inx + 1), query(mid + 1, end, i, j, 2 * inx + 2)); 
+	}
+
+	vector<long long> segmentTree;
+	int size;
+}; 
+
+int N, M, K;
+vector<int> A;
+vector<vector<long long>> dp;
+vector<SegmentTree> rmq;
+ 
+long long dfs(int i, int j) {
+	if(j == 0)
+	{
+		if(N <= i + K - 1)
+		{
+			dp[i][j] = A[i - 1];
+		}
+		else
+		{
+			dp[i][j] = -INF;
+		}
+		rmq[j].update(i, dp[i][j]);
+		return dp[i][j];
+	}
+	if(i >= N) return -INF;
+	if(dp[i][j] != -1) return dp[i][j];
+	dp[i][j] = rmq[j - 1].query(i + 1, min(i + K, N)) + (i != 0 ? A[i - 1] : 0);
+	rmq[j].update(i, dp[i][j]);
+	return dp[i][j];
+}
+
+int main() {
+	cin >> N >> M >> K;
+	A = vector<int>(N, 0);
+	dp = vector<vector<long long>>(N + 1, vector<long long>(M + 1, -1));
+	rmq = vector<SegmentTree>(M + 1, vector<long long>(N + 1));
+	for(int i = 0; i < N; i++) cin >> A[i];
+	for(int j = 0; j <= M; j++)
+	{
+		for(int i = N; i >= 0; i--) dfs(i, j);
+	}
+	cout << max(dp[0][M], -1LL) << endl;
+	return 0;
+}
