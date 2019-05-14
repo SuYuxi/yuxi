c26c99f202f1097ee405e8b11e2f83fe04cba6bf
yuxi <soration2099@gmail.com>
2019-05-14 15:32:36 +0900

    update

--- Classical/SuffixArray.cpp
+++ Classical/SuffixArray.cpp
@@ -93,91 +93,9 @@ vector<int> buildLCP(const string& txt, const vector<int>& suffixArr) { // longe
 	return LCP;
 }
 
-class Solution {
-public:
-    string longestDupSubstring(string S) {
-		int size = S.size();
-		vector<int> suffixArr = buildSuffixArray(S);
-		vector<int> lcp = buildLCP(S, suffixArr);
-
-		int start = 0;
-		int Max = 0;
-		
-		for(int i = 0; i < size; i++)
-		{
-			if(lcp[i] > Max)
-			{
-				Max = lcp[i];
-				start = suffixArr[i];
-			}
-		}
-
-		return S.substr(start, Max);
-    }
+int main() {
+	string txt = "banana";
+	vector<int> suffixArr = buildSuffixArray(S);
+	vector<int> lcp = buildLCP(S, suffixArr);
 };
 
-//Binary Search and Rabin-Karp(Rolling Hash)
-class Solution {
-public:
-	Solution () {
-		MOD = 1e9 + 7;//prime
-		p = 113;//prime
-		pinv = pow(p, MOD - 2, MOD); //the mod inverse of p(MOD should be prime), by FERMAT's little therorem
-	}
-
-    string longestDupSubstring(string S) {
-		int size = S.size();
-		int low = 0, high = size; //length of duplicated substring
-		int res = -1;
-		while(low < high)
-		{
-			int mid = (low + 1 + high) / 2;
-			int pos = search(S, mid);
-			if(pos == -1)
-			{
-				high = mid - 1;
-			}
-			else
-			{
-				low = mid;
-				res = pos;
-			}
-		}
-		return res == -1 ? "" : S.substr(res, low);
-    }
-
-private:
-	long MOD;
-	int p;
-	int pinv;
-	int search(string& S, int len) {
-		if(len > S.size()) return -1;
-		long hash = 0;
-		long power = 1;
-		for(int i = 0; i < len; i++)
-		{
-			hash = (hash + S[i] * power) % MOD;
-			power = (power * p) % MOD;
-		}
-		unordered_map<long, int> seen; //in case of hash collision
-		seen[hash] = 0;
-		power = (power * pinv) % MOD;
-		for(int i = len; i < S.size(); i++)
-		{
-			hash -= S[i - len];
-			hash *= pinv;
-			hash += S[i] * power;
-			hash %= MOD;
-			if(seen.count(hash) && S.substr(seen[hash], len) == S.substr(i - len + 1, len)) return i - len + 1; //there's still possibility of third hash collision. 
-			seen[hash] = i - len + 1;
-		}
-		return -1;
-	}
-
-	int pow(int x, int y, int m) {
-		if(y == 0) return 1;
-		long p = pow(x, y / 2, m);
-		p = (p * p) % m;
-		return (y % 2 == 0) ? p : (p * x) % m;
-	}
-};
