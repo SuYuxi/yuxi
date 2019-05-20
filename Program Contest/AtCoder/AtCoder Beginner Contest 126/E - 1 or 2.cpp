34075f0540dbd1d76bb242e1daa4d9e34c4b32d2
yuxi <soration2099@gmail.com>
2019-05-21 00:58:53 +0900

    update

--- Program Contest/AtCoder/AtCoder Beginner Contest 126/E - 1 or 2.cpp
+++ Program Contest/AtCoder/AtCoder Beginner Contest 126/E - 1 or 2.cpp
@@ -1,17 +1,70 @@
+//union find
+//DSU Disjoint Set Union
 #include <iostream>
-#include <string>
-
+#include <vector>
 using namespace std;
+
+class DSU {
+public:
+	DSU(int _size) : size(_size) {
+		rank = vector<int>(size + 1, 1);
+		parent = vector<int>(size + 1);
+		for(int i = 0; i < size; i++)
+		{
+			parent[i] = i;
+		}
+	}
+
+	int find(int i) {
+		if(parent[i] != i)
+		{
+			parent[i] = find(parent[i]);
+		}
+		return parent[i];
+	}
+
+	bool Union(int i, int j) {
+		int ri = find(i), rj = find(j);
+		if(ri == rj) return false;
+		if(rank[ri] > rank[rj])
+		{
+			parent[rj] = ri;
+		}
+		else if (rank[ri] < rank[rj])
+		{
+			parent[ri] = rj;
+		}
+		else
+		{
+			parent[rj] = ri;
+			rank[ri] += 1;
+		}
+		return true;
+	}
+
+private:
+	int size;
+	vector<int> rank;
+	vector<int> parent;
+};
+
 int main()
 {
-	int N, K;
-	string S;
-	cin >> N >> K;
-	cin >> S;
-	if(S[K - 1] >= 'A' && S[K - 1] <= 'Z')
+	int N, M;
+	cin >> N >> M;
+	vector<int> X(M), Y(M), Z(M);
+	for(int i = 0; i < M; i++)
 	{
-		S[K - 1] += 32;
+		cin >> X[i] >> Y[i] >> Z[i];
 	}
-	cout << S << endl;
+
+	DSU dsu(N);
+	int ans = N;
+	for(int i = 0; i < M; i++)
+	{
+		ans -= dsu.Union(X[i], Y[i]);
+	}
+
+	cout << ans << endl;
 	return 0;
 }
