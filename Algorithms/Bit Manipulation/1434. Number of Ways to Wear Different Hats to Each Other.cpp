7467c0bc8aab6cc9ab70e3b941ec47d864971064
Yuxi <SuYuxi@users.noreply.github.com>
2020-05-03 16:02:26 +0900

    Update 1434. Number of Ways to Wear Different Hats to Each Other.cpp

--- Algorithms/Bit Manipulation/1434. Number of Ways to Wear Different Hats to Each Other.cpp
+++ Algorithms/Bit Manipulation/1434. Number of Ways to Wear Different Hats to Each Other.cpp
@@ -10,10 +10,10 @@ public:
             }
         }
         vector<vector<int>> dp(41, vector<int>(1024, -1));
-        return backtrack(peoples, dp, hats.size(), 0, 1);
+        return dfs(peoples, dp, hats.size(), 0, 1);
     }
     
-    int backtrack(vector<int>& peoples, vector<vector<int>>& dp, int people, int hash, int inx) {
+    int dfs(vector<int>& peoples, vector<vector<int>>& dp, int people, int hash, int inx) {
         if(inx > 40)
         {
             return (hash == (1 << people) - 1) ? 1 : 0;
@@ -22,13 +22,13 @@ public:
         {
             return dp[inx][hash];
         }
-        int number = backtrack(peoples, dp, people, hash, inx + 1);
+        int number = dfs(peoples, dp, people, hash, inx + 1);
         for(int p = 0; p < people; ++p)
         {
             if(hash & (1 << p)) continue;
             if(peoples[inx] & (1 << p))
             {
-                number += backtrack(peoples, dp, people, hash | (1 << p), inx + 1);
+                number += dfs(peoples, dp, people, hash | (1 << p), inx + 1);
                 number %= mod;
             }
         }
