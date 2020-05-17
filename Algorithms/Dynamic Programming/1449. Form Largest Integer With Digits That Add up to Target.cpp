52c602d4989babebe209fd921f75cb7e213c2fc3
Yuxi <SuYuxi@users.noreply.github.com>
2020-05-17 14:45:24 +0900

    Update 1449. Form Largest Integer With Digits That Add up to Target.cpp

--- Algorithms/Dynamic Programming/1449. Form Largest Integer With Digits That Add up to Target.cpp
+++ Algorithms/Dynamic Programming/1449. Form Largest Integer With Digits That Add up to Target.cpp
@@ -1,34 +1,23 @@
+//bottom-up
 class Solution {
 public:
     string largestNumber(vector<int>& cost, int target) {
-        unordered_map<int, string> dp;
-        backTracking(cost, dp, target);
-        return dp[target];
-    }
-    
-    void backTracking(vector<int>& cost, unordered_map<int, string>& dp, int target) {
-        string maxPaint = "0";
-        for(int num = 1; num <= 9; ++num)
+        vector<string> dp(target + 1, "0");
+        dp[0] = "";
+        for(int t = 1; t <= target; ++t)
         {
-            int c = cost[num - 1];
-            if(c > target) continue;
-            if(c == target && maxPaint.size() == 1 && maxPaint < to_string(num))
+            for(int num = 1; num <= 9; ++num)
             {
-                maxPaint = to_string(num);
-                continue;
-            }
-            if(dp.count(target - c) == 0)
-            {    
-                backTracking(cost, dp, target - c);
-            }
-            if(dp[target - c] != "0")
-            {
-                string xxx = to_string(num) + dp[target - c];
-                if(xxx.size() > maxPaint.size() || (xxx.size() == maxPaint.size() && xxx > maxPaint)) {
-                    maxPaint = xxx;
+                int c = cost[num - 1];
+                if(c <= t && dp[t - c] != "0")
+                {
+                    if(dp[t].size() <= dp[t - c].size() + 1)
+                    {
+                        dp[t] = to_string(num) + dp[t - c];
+                    }
                 }
             }
         }
-        dp[target] = maxPaint;
+        return dp[target];
     }
 };
