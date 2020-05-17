ca390aa97e1c809a0ae6eb5b31ad68ad668b6288
Yuxi <SuYuxi@users.noreply.github.com>
2020-05-17 15:52:51 +0900

    Update 1449. Form Largest Integer With Digits That Add up to Target.cpp

--- Algorithms/Dynamic Programming/1449. Form Largest Integer With Digits That Add up to Target.cpp
+++ Algorithms/Dynamic Programming/1449. Form Largest Integer With Digits That Add up to Target.cpp
@@ -21,3 +21,32 @@ public:
         return dp[target];
     }
 };
+
+//top-down
+class Solution {
+public:
+    string largestNumber(vector<int>& cost, int target) {
+        dp = vector<string>(target + 1);
+        return dfs(cost, target);
+    }
+        
+private:
+    string dfs(vector<int>& cost, int target) {
+        if(target <= 0) return target == 0 ? "" : "0";
+        if(!dp[target].empty()) return dp[target];
+        string res = "0";
+        for(int num = 1; num <= 9; ++num)
+        {
+            int c = cost[num - 1];
+            string str = dfs(cost, target - c);
+            if(str != "0" && str.size() + 1 >= res.size())
+            {
+                res = to_string(num) + str;
+            }
+        }
+        dp[target] = res;
+        return dp[target];
+    }
+
+    vector<string> dp;
+};
