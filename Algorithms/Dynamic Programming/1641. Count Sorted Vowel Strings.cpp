e1304de3ac16fafdc6b1352e3371c6a8a7225811
Yuxi <SuYuxi@users.noreply.github.com>
2022-07-14 22:14:39 +0900

    Create 1641. Count Sorted Vowel Strings.cpp

---
+++ Algorithms/Dynamic Programming/1641. Count Sorted Vowel Strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countVowelStrings(int n) {
+		vector<vector<int>> dp(n + 1, vector<int>(6, 0));
+		for(int i = 1; i <= n; ++i) {
+			for(int j = 1; j <= 5; ++j) {
+				dp[i][j] = dp[i][j - 1] + (i > 1 ? dp[i-1][j] : 1);
+            } 
+		}
+		return dp[n][5];
+    }
+};
