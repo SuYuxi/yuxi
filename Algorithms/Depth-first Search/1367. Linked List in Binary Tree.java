81e0a138c3e4c31ed3f5f9e0cfb6f985673e9cea
Yuxi <SuYuxi@users.noreply.github.com>
2022-08-19 16:17:32 +0900

    Update 1367. Linked List in Binary Tree.java

--- Algorithms/Depth-first Search/1367. Linked List in Binary Tree.java
+++ Algorithms/Depth-first Search/1367. Linked List in Binary Tree.java
@@ -36,3 +36,31 @@ class Solution {
         return head.val == root.val && (dfs(head.next, root.left) || dfs(head.next, root.right));
     }
 }
+
+//KMP
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        List<Integer> A = new ArrayList(), dp = new ArrayList();
+        A.add(head.val);
+        dp.add(0);
+        int i = 0;
+        head = head.next;
+        while (head != null) {
+            while (i > 0 && head.val != A.get(i))
+                i = dp.get(i - 1);
+            if (head.val == A.get(i)) ++i;
+            A.add(head.val);
+            dp.add(i);
+            head = head.next;
+        }
+        return dfs(root, 0, A, dp);
+    }
+
+    private boolean dfs(TreeNode root, int i, List<Integer> A, List<Integer> dp) {
+        if (root == null) return false;
+        while (i > 0 && root.val != A.get(i))
+            i = dp.get(i - 1);
+        if (root.val == A.get(i)) ++i;
+        return i == dp.size() || dfs(root.left, i, A, dp) || dfs(root.right, i, A, dp);
+    }
+}
