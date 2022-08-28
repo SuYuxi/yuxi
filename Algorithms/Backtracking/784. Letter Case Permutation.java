b57be7f2024233af5a3715933e1f224903e1d8f6
Yuxi <SuYuxi@users.noreply.github.com>
2022-08-29 02:35:56 +0900

    Create 784. Letter Case Permutation.java

---
+++ Algorithms/Backtracking/784. Letter Case Permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, 0, S.toCharArray());
+        return ans;
+    }
+    public void backtrack(List<String> ans, int i, char[] S){
+        if(i==S.length)
+            ans.add(new String(S));
+        else{
+            if(Character.isLetter(S[i])){ //If it's letter
+                S[i] = Character.toUpperCase(S[i]);
+                backtrack(ans, i+1, S); //Upper case branch
+                S[i] = Character.toLowerCase(S[i]);
+                backtrack(ans, i+1, S); //Lower case branch
+            }
+            else
+                backtrack(ans, i+1, S); 
+        }
+    }
+}
