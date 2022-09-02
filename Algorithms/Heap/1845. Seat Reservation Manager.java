832be597744ce8c7c24e7f5a0766074f37af4920
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-02 11:57:34 +0900

    Create 1845. Seat Reservation Manager.java

---
+++ Algorithms/Heap/1845. Seat Reservation Manager.java
@@ -0,0 +1,24 @@
+class SeatManager {
+    private Queue<Integer> pq;
+    public SeatManager(int n) {
+        pq = new PriorityQueue();
+        for(int i = 1; i <= n; ++i) {
+            pq.offer(i);
+        }
+    }
+    
+    public int reserve() {
+        return pq.poll();
+    }
+    
+    public void unreserve(int seatNumber) {
+        pq.offer(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
