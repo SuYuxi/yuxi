81973dbc6430efcc507875a2f7549cfa0f1d2cd6
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-12 19:42:09 +0900

    Create 1608. Special Array With X Elements Greater Than or Equal X.java

---
+++ Algorithms/Binary Search/1608. Special Array With X Elements Greater Than or Equal X.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            int n = nums.length-i;
+            boolean cond1 =  n<=nums[i];
+            boolean cond2 = (i-1<0) || (n>nums[i-1]);
+            if (cond1 && cond2) return n;
+        }
+        return -1;
+    }
+}
