4f8667f7de1c133f089744fac712f3f5d569e9d9
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-15 15:09:13 +0900

    Create 1346. Check If N and Its Double Exist.java

---
+++ Algorithms/Hash Table/1346. Check If N and Its Double Exist.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet();
+        for(int num : arr) {
+            if(set.contains(2 * num) || ((num & 1) == 0 && set.contains(num / 2))) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
