98fd0809bae3097eaf2180af833e45528cdf1e61
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-26 15:46:40 +0900

    Update 1129. Shortest Path with Alternating Colors.java

--- Algorithms/Graph/1129. Shortest Path with Alternating Colors.java
+++ Algorithms/Graph/1129. Shortest Path with Alternating Colors.java
@@ -1,46 +1,42 @@
- public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-        // Two sets one for blu and another for red
+class Solution {
+    public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
         Set<Integer>[][] graph = new HashSet[2][n];
+        int[][] steps = new int[2][n];
         for (int i = 0; i < n; i++) {
             graph[0][i] = new HashSet<>();
             graph[1][i] = new HashSet<>();
         }
-        // red edges in 0 - col
-        for (int[] re : red_edges) {
-            graph[0][ re[0] ].add(re[1]);
+        for(int i = 0; i < 2; ++i) {
+            Arrays.fill(steps[i], 2 * n);
         }
-        // blu edges in 1 - col
-        for (int[] blu : blue_edges) {
-            graph[1][ blu[0] ].add(blu[1]);
+        steps[0][0] = 0;
+        steps[1][0] = 0;
+        for(int[] edge : red_edges) {
+            graph[0][edge[0]].add(edge[1]);
         }
-        int[][] res = new int[2][n];
-        // Zero edge is always accessible to itself - leave it as 0
-        for (int i = 1; i < n; i++) {
-            res[0][i] = 2 * n;
-            res[1][i] = 2 * n;
+        for(int[] edge : blue_edges) {
+            graph[1][edge[0]].add(edge[1]);
         }
-        // Q entries are vert with a color (up to that point)
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0}); // either with red
-        q.offer(new int[] {0, 1}); // or with blue
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int vert = cur[0];
-            int colr = cur[1];
-            // No need to keep track of level up to now
-            // only need to keep what color - and the length
-            // is automatically derived from previous node
-            for (int nxt : graph[1 - colr][vert]) {
-                if (res[1 - colr][nxt] == 2 * n) {
-                    res[1 - colr][nxt] = 1 + res[colr][vert];
-                    q.offer(new int[] {nxt, 1 - colr});
+
+        Queue<int[]> queue = new LinkedList();
+        queue.add(new int[]{0, 0});
+        queue.add(new int[]{0, 1});
+        while(!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int cur = node[0];
+            int color = node[1];
+            for(int next : graph[color][cur]) {
+                if(steps[1 - color][next] == 2 * n) {
+                    queue.add(new int[]{next, 1 - color});
+                    steps[1 - color][next] = steps[color][cur] + 1;
                 }
             }
         }
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            int t = Math.min(res[0][i], res[1][i]);
-            ans[i] = (t == 2 * n) ? -1 : t;
+        int[] res = new int[n];
+        for(int i = 0; i < n; ++i) {
+            int t = Math.min(steps[0][i], steps[1][i]);
+            res[i] = (t == 2 * n) ? -1 : t;
         }
-        return ans;
+        return res;
     }
+}
