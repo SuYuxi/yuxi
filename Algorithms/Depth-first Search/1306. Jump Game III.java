fe6b51745f0225376f7b31319a2df452937b7c35
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-27 19:01:46 +0900

    Create 1306. Jump Game III.java

---
+++ Algorithms/Depth-first Search/1306. Jump Game III.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if(start < 0 || start >= arr.length || arr[start] < 0) return false;
+        if(arr[start] == 0) return true;
+        int jump = arr[start];
+        arr[start] = -arr[start];
+        return canReach(arr, start - jump) || canReach(arr, start + jump);
+    }
+}
