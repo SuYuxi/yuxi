4e90897cbd000b9fc91ec095f777ea6572040b06
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-30 13:00:18 +0900

    Update 886. Possible Bipartition.java

--- Algorithms/Graph/886. Possible Bipartition.java
+++ Algorithms/Graph/886. Possible Bipartition.java
@@ -10,17 +10,17 @@ class Solution {
         for(int i = 0; i < n; ++i) {
             if(colors[i] == 0) {
                 colors[i] = 1;
-                if(!dfs(graph, colors, i, 1)) return false;
+                if(!dfs(graph, colors, i)) return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(List<Integer>[] graph, int[] colors, int pos, int color) {
+    private boolean dfs(List<Integer>[] graph, int[] colors, int pos) {
         for(int next : graph[pos]) {
-            if(colors[next] == color) return false;
+            if(colors[next] == colors[pos]) return false;
             if(colors[next] == 0) {
-                colors[next] = 3 - color;
+                colors[next] = 3 - colors[pos];
                 if(!dfs(graph, colors, next, colors[next])) return false;
             }
         }
