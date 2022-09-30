75f7c4020fad1ade276e2961a38a1c07ca7ccfbf
Yuxi <SuYuxi@users.noreply.github.com>
2022-09-30 12:52:04 +0900

    Create 886. Possible Bipartition.java

---
+++ Algorithms/Graph/886. Possible Bipartition.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        List<Integer>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; ++i) graph[i] = new ArrayList<Integer>();
+        int[] colors = new int[n];
+        for(int[] dislike : dislikes) {
+            graph[dislike[0] - 1].add(dislike[1] - 1);
+            graph[dislike[1] - 1].add(dislike[0] - 1);
+        }
+        for(int i = 0; i < n; ++i) {
+            if(colors[i] == 0) {
+                colors[i] = 1;
+                if(!dfs(graph, colors, i, 1)) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(List<Integer>[] graph, int[] colors, int pos, int color) {
+        for(int next : graph[pos]) {
+            if(colors[next] == color) return false;
+            if(colors[next] == 0) {
+                colors[next] = 3 - color;
+                if(!dfs(graph, colors, next, colors[next])) return false;
+            }
+        }
+        return true;
+    }
+}
